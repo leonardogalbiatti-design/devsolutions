0c41a910982ea56443b6c1adbb65ac2a9af4f236
leonardogalbiatti-design <leonardo.galbiatti@edu.senai.br>
2026-08-20 10:46:30 -0300

    correção funcionalidades

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
  document.getElementById("form-contato").addEventListener("submit", function(evento) {
  evento.preventDefault();
 
- var nome document.getElementById("nome").value;
- var email document.getElementById("email").value;
+ var nome = document.getElementById("nome").value;
+ var email = document.getElementById("email").value;
 
- if (nome === || email === "") {
+ if (nome === " "|| email === "") {
  alert("Por favor, preencha todos os campos.");
  } else {
 
